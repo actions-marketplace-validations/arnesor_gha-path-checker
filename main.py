c7cd4a8e325ea196f7b3131a6adfd366499ff8af
Arne Sørli <arnesor@users.noreply.github.com>
2021-09-24 21:33:21 +0200

    First try

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 import os
-import requests  # noqa We are just importing this to prove the dependency installed correctly
+import sys
+from pathlib import Path
 
 
 def main():
-    my_input = os.environ["INPUT_MYINPUT"]
+    allow_non_ascii = os.environ["INPUT_NONASCII"] == "true"
 
-    my_output = f"Hello {my_input}"
+    root = Path(".")
+    paths = [str(item) for item in root.rglob("*")]
 
-    print(f"::set-output name=myOutput::{my_output}")
+    # Check for paths with spaces
+    error_paths = [item for item in paths if " " in item]
+
+    if not allow_non_ascii:
+        with_non_ascii = [item for item in paths if not item.isascii()]
+        error_paths.extend(with_non_ascii)
+
+    sorted_error_paths = sorted(set(error_paths))
+
+    if sorted_error_paths:
+        print("The following paths have unwanted characters:")
+        print(*sorted_error_paths, sep="\n")
+        print(f"::set-output name=errors::{len(sorted_error_paths)}")
+        sys.exit(1)
+    else:
+        print("Paths OK")
+        print(f"::set-output name=errors::{len(sorted_error_paths)}")
+        sys.exit(0)
 
 
 if __name__ == "__main__":
